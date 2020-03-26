72d434956ab4093ef434e0342a24d4a399c5f245
Gavin Mogan <git@gavinmogan.com>
2020-03-26 12:01:05 -0700

    Add setup.py for easier install (No need to figure out paths)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="rest_auth_provider",
+    version="0.0.1",
+    py_modules=['rest_auth_provider'],
+    description="Password Provider for Synapse fetching data from a REST endpoint",
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[],
+)
+
